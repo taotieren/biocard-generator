67999de4fdedc823a6b9d80cd6417051d28d8a98
littlegragon <705764065@qq.com>
2019-02-01 15:47:29 +0800

    feat: 修改构建生成文件位置

--- webpack.config.pages.js
+++ webpack.config.pages.js
@@ -2,6 +2,6 @@ const config = require('./webpack.config.base');
 const path = require('path');
 config.output = {
   filename: 'bundle.js',
-  path: path.resolve(__dirname, 'public/pages'),
+  path: path.resolve(__dirname, ''),
 }
 module.exports = config;
